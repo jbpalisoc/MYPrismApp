9394480a0aa845438b7d11c05186ebd7372cd75d
Jason Palisoc <jasonpalisoc27@gmail.com>
2021-01-18 09:57:06 +0800

    Add project files.

---
+++ Project2/ModuleName.cs
@@ -0,0 +1,20 @@
+﻿using Project2.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Project2
+{
+    public class ModuleName : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
